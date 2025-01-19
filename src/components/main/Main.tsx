f46d110114ff28bb6b479d8891713d87866c399b
imran khan <imranloveh01@gmail.com>
2025-01-19 14:00:21 +0500

    Enhance TypeScript configuration and update Main component layout
    - Added incremental compilation support in tsconfig.app.json and tsconfig.node.json.
    - Removed the noUncheckedSideEffectImports option from both TypeScript configuration files.
    - Updated Main component to include a new Button for user interaction and improved layout structure for better presentation.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,12 +1,20 @@
+import { Button } from "../ui/button"
+
 const Main = () => {
   return (
-    <div className='flex items-center justify-center bg-[url("/bg.svg")] bg-cover bg-bottom bg-no-repeat min-h-screen'>
+    <div className='flex items-center justify-center bg-[url("/bg.svg")] bg-cover bg-bottom bg-no-repeat min-h-screen flex-col'>
         <h1 className="text-4xl font-bold">
             Welcome to <span className="bg-[url('/text-overlay.svg')] bg-cover bg-center bg-no-repeat">AssanGPT</span>
+        </h1>
             <p className="text-sm font-normal my-5 text-center">
               Your AI-powered assistant for all your needs
             </p>
-        </h1>
+
+            <div className="flex items-center justify-center">
+              <Button>
+                Get Started
+              </Button>
+            </div>
     </div>
   )
 }
